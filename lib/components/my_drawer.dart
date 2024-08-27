ca03d0871166d486d47fc7d1b38c8e107e2c890d
ZakyTm <zakaria.t149919@gmail.com>
2024-08-27 20:59:39 +0100

    Add logo to MyDrawer component

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -7,6 +7,16 @@ class MyDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 50),
+        child: Column(
+          children: [
+            //Logo
+            Icon(Icons.message,
+                size: 60, color: Theme.of(context).colorScheme.primary),
+          ],
+        ),
+      ),
     );
   }
 }

f41cec40f0601d50fbbfbab274c94680106f6e45
ZakyTm <zakaria.t149919@gmail.com>
2024-08-27 20:30:19 +0100

    Refactor authentication flow and add AuthGate widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void logout() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+            },
+          ),
+        ],
       ),
     );
   }

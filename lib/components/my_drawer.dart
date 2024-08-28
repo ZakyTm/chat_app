bbf3abc673bf82d7c8d0acccc0303a4dff96c517
ZakyTm <zakaria.t149919@gmail.com>
2024-08-28 20:22:02 +0100

    refactor: Improve MyDrawer component layout and add navigation options

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -10,10 +10,55 @@ class MyDrawer extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.only(top: 50),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            //Logo
-            Icon(Icons.message,
-                size: 60, color: Theme.of(context).colorScheme.primary),
+            Column(
+              children: [
+                //Logo drawer header
+                DrawerHeader(
+                  child: Center(
+                    child: Icon(Icons.message,
+                        color: Theme.of(context).colorScheme.primary, size: 60),
+                  ),
+                ),
+
+                const SizedBox(height: 50),
+                // Home list tile
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: ListTile(
+                    title: const Text("H O M E"),
+                    leading: const Icon(Icons.home),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+
+                // setting list tile
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: ListTile(
+                    title: const Text("S E T T I N G S"),
+                    leading: const Icon(Icons.settings),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            //logout list tile
+            Padding(
+              padding: const EdgeInsets.only(left: 25, bottom: 30),
+              child: ListTile(
+                title: const Text("L O  G O U T"),
+                leading: const Icon(Icons.logout),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
           ],
         ),
       ),

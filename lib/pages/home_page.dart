72b96437fdc7df61cbb38b3ac59fed9d19693f36
ZakyTm <zakaria.t149919@gmail.com>
2024-08-31 19:15:44 +0100

    chore: Add user list to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,16 @@
 import 'package:chat_app/components/my_drawer.dart';
+import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:chat_app/services/chat/chat_service.dart';
 
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  //chat & auth service
+
+  final ChatService chatService = ChatService();
+  final AuthService authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +19,31 @@ class HomePage extends StatelessWidget {
         title: const Text("Home"),
       ),
       drawer: const MyDrawer(),
+      body: buildUserList(),
+    );
+  }
+
+  //build a list of users execpt for the current logged in user
+  Widget _buildUserList() {
+    return StreamBuilder(
+      stream: chatService.getUserStream(),
+      builder: (context, snapshot) {
+        // if there is an error
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        // loading ..
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator(
+            color: Colors.blue,
+            semanticsLabel: "Loading",
+          );
+        }
+
+        // if there is a data
+        return ListView(children: []);
+      },
     );
   }
 }

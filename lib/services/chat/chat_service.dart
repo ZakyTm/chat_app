1839b457f3ed4b39bf565f8adb4212d177025858
ZakyTm <zakaria.t149919@gmail.com>
2024-09-04 19:07:27 +0100

    Add message sending functionality to ChatService

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/models/message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -28,18 +29,32 @@ class ChatService {
   //get chat by id
 
   // send a message
-  Future<void> sendMessage(String receiverId, message) async {
+  Future<void> sendMessage(String receiverID, message) async {
     // get current user info
     final String currentUserID = auth.currentUser!.uid;
     final String currentUserEmail = auth.currentUser!.email!;
     final Timestamp timestamp = Timestamp.now();
 
     // creat a new message
-    
+    Message newMessage = Message(
+        senderID: currentUserID,
+        senderEmail: currentUserEmail,
+        receiverID: receiverID,
+        message: message,
+        timestamp: timestamp);
 
     // custruct chat room ID for the two users (sorted to ensure uniqueness)
-
+    List<String> ids = [currentUserID, receiverID];
+    ids.sort(); // sort the ids just to know the order of the messages which one
+    //is the first and what comes after you send your message !!
+    String chatRoomID = ids.join(" _ ");
     //add new message to database
+
+    await firestore
+        .collection("chat_room")
+        .doc()
+        .collection("messages")
+        .add(newMessage.toMap());
   }
 
   // get messages by chat id

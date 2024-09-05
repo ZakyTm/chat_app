aa6d80865e6b626a97840787b96d21c07eec03f8
ZakyTm <zakaria.t149919@gmail.com>
2024-09-05 20:42:09 +0100

    feat: Add message sending functionality to ChatService

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,9 +1,18 @@
+import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:chat_app/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
   final String receiverEmail;
-  const ChatPage({super.key, required this.receiverEmail});
+  ChatPage({super.key, required this.receiverEmail});
 
+  // text controller for the message
+  final TextEditingController messageController = TextEditingController();
+
+  //chat & auth services instances
+
+  final ChatService chatService = ChatService();
+  final AuthService authService = AuthService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(

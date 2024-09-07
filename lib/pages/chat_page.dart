d22d4a687f080881d9fbdc5b8ec8996a712667fd
ZakyTm <zakaria.t149919@gmail.com>
2024-09-08 00:20:53 +0100

    finishing with the buildMessageInput widget

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/components/my_textfiled.dart';
 import 'package:chat_app/services/auth/auth_service.dart';
 import 'package:chat_app/services/chat/chat_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -36,9 +37,13 @@ class ChatPage extends StatelessWidget {
       ),
       body: Column(
         children: [
+          // display all messages
           Expanded(
             child: _buildMessageList(),
           ),
+
+          // user input
+          buildMessageInput(),
         ],
       ),
     );
@@ -74,5 +79,27 @@ class ChatPage extends StatelessWidget {
   Widget buildMessageItem(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     return Text(data["message"]);
+  }
+
+  // build message intpu
+  Widget buildMessageInput() {
+    return Row(
+      children: [
+        // text field should take up most of the space
+        Expanded(
+          child: MyTextfiled(
+            controller: messageController,
+            hintText: "Type a message",
+            obscureText: false,
+          ),
+        ),
+
+        // send button
+        IconButton(
+          onPressed: sendMessage,
+          icon: const Icon(Icons.send),
+        ),
+      ],
+    );
   }
 }
